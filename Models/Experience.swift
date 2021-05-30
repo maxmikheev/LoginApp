8c48739d886146b5405cbc8714a6cac57b543e0e
Maksim Mikheev <MMikheev@yotateam.com>
2021-05-30 19:33:02 +0300

    Небольшие изменения ui

---
+++ Models/Experience.swift
@@ -0,0 +1,16 @@
+//
+//  Experience.swift
+//  LoginApp
+//
+//  Created by Максим Михеев on 30.05.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+struct experience {
+    let companyName: String
+    let service: Float
+    let Position: String
+    let responsibilities: [Any]
+}
---
+++ Models/Personal info.swift
@@ -0,0 +1,18 @@
+//
+//  Personal info.swift
+//  LoginApp
+//
+//  Created by Максим Михеев on 30.05.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+struct personalInfo {
+    let name: String
+    let surname: String
+    let age: Float
+    let city: String
+    let university: String
+    
+}
